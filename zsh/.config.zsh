303e18be2e15e51ec9b07e1910a481772959388f
kylec <kylec@aweber.com>
2017-07-26 22:03:53 -0400

    Remove oh-my-zsh and add custom completion

---
+++ zsh/.config.zsh
@@ -0,0 +1,202 @@
+#                      _____                  __
+#    _________  ____  / __(_)___ _____  _____/ /_
+#   / ___/ __ \/ __ \/ /_/ / __ `/_  / / ___/ __ \
+# _/ /__/ /_/ / / / / __/ / /_/ / / /_(__  ) / / /
+#(_)___/\____/_/ /_/_/ /_/\__, (_)___/____/_/ /_/
+#                        /____/
+# Enable vi editing mode and show in prompt
+function zle-keymap-select {
+  VIMODE="${${KEYMAP/vicmd/ M:command}/(main|viins)/}"
+  zle reset-prompt
+}
+
+zle -N zle-keymap-select
+bindkey -v
+set show-mode-in-prompt on
+
+# Vi mode breaks uparrow completion. Fix it.
+# start typing + [Up-Arrow] - fuzzy find history forward
+if [[ "${terminfo[kcuu1]}" != "" ]]; then
+  autoload -U up-line-or-beginning-search
+  zle -N up-line-or-beginning-search
+  bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
+fi
+# start typing + [Down-Arrow] - fuzzy find history backward
+if [[ "${terminfo[kcud1]}" != "" ]]; then
+  autoload -U down-line-or-beginning-search
+  zle -N down-line-or-beginning-search
+  bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
+fi
+
+# Enable changing cursor shapes and blinking
+function print_dcs
+{
+  print -n -- "\EP$1;\E$2\E\\"
+}
+
+function set_cursor_shape
+{
+  if [ -n "$TMUX" ]; then
+    # tmux will only forward escape sequences to the terminal if surrounded by
+    # a DCS sequence
+    print_dcs tmux "\E]50;CursorShape=$1\C-G"
+  else
+    print -n -- "\E]50;CursorShape=$1\C-G"
+  fi
+}
+
+function zle-keymap-select zle-line-init
+{
+  case $KEYMAP in
+    vicmd)
+      set_cursor_shape 0 # block cursor
+      ;;
+    viins|main)
+      set_cursor_shape 1 # line cursor
+      ;;
+  esac
+  zle reset-prompt
+  zle -R
+}
+
+function zle-line-finish
+{
+  set_cursor_shape 0 # block cursor
+}
+
+zle -N zle-line-init
+zle -N zle-line-finish
+zle -N zle-keymap-select
+
+export GREP_OPTIONS='--binary-files=without-match --ignore-case'
+export ZSH_AUTOSUGGEST_USE_ASYNC='1'
+
+# ctrl-\ executes suggestion
+bindkey '^\' autosuggest-execute
+# ctrl-] accepts suggestion
+bindkey '^]' autosuggest-execute
+
+bindkey '^[[A' up-line-or-search                                                
+bindkey '^[[B' down-line-or-search
+
+# History Settings (big history for use with many open shells and no dups)
+# Different History files for root and standard user
+HISTFILE=$HOME/.zsh_history
+SAVEHIST=10000
+HISTSIZE=12000
+setopt share_history append_history extended_history hist_no_store hist_ignore_all_dups hist_ignore_space
+
+# 2x control is completion from history!!!
+zle -C hist-complete complete-word _generic
+zstyle ':completion:hist-complete:*' completer _history
+bindkey '^X^X' hist-complete
+
+# If a command is issued that can’t be executed as a normal command, and the command is the name of a directory, perform the cd command to that directory.
+setopt AUTO_CD
+
+# Treat  the ‘#’, ‘~’ and ‘^’ characters as part of patterns for filename generation, etc.  (An initial unquoted ‘~’ always produces named directory expansion.)
+setopt EXTENDED_GLOB
+
+# If a pattern for filename generation has no matches, print an error, instead of leaving it unchanged in the argument  list. This also applies to file expansion of an initial ‘~’ or ‘=’.
+setopt NOMATCH
+
+# no Beep on error in ZLE.
+setopt NO_BEEP
+
+# Remove  any right prompt from display when accepting a command line. This may be useful with terminals with other cut/paste methods.
+setopt TRANSIENT_RPROMPT
+
+# If unset, the cursor is set to the end of the word if completion is started. Otherwise it stays there and completion is done from both ends.
+setopt COMPLETE_IN_WORD
+
+# Make cd push the old directory onto the directory stack.
+setopt AUTO_PUSHD
+
+# Don’t push multiple copies of the same directory onto the directory stack.
+setopt PUSHD_IGNORE_DUPS
+
+# DON NOT Allow ‘>’ redirection to truncate existing files, and ‘>>’ to create files. Otherwise ‘>!’ or ‘>|’ must be used to truncate  a file, and ‘>>!’ or ‘>>|’ to create a file.
+setopt no_clobber
+
+# tab completion configuration
+
+# add an autoload function path, if directory exists
+# http://www.zsh.org/mla/users/2002/msg00232.html
+functionsd="$HOME/.zsh-functions.d"
+if [[ -d "$functionsd" ]] {
+    fpath=( $functionsd $fpath )
+    autoload -U $functionsd/*(:t)
+}
+
+# load completions system
+zmodload -i zsh/complist
+
+# auto rehash commands
+# http://www.zsh.org/mla/users/2011/msg00531.html
+zstyle ':completion:*' rehash true
+
+# for all completions: menuselection
+zstyle ':completion:*' menu select=1
+
+# for all completions: grouping the output
+zstyle ':completion:*' group-name ''
+
+# for all completions: color
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+# for all completions: selected item
+#zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS} ma=0\;47
+
+# completion of .. directories
+zstyle ':completion:*' special-dirs true
+
+# fault tolerance
+zstyle ':completion:*' completer _complete _correct _approximate
+# (1 error on 3 characters)
+zstyle -e ':completion:*:approximate:*' max-errors 'reply=( $(( ($#PREFIX+$#SUFFIX)/3 )) numeric )'
+
+# case insensitivity
+#zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+zstyle ":completion:*" matcher-list 'm:{A-Zöäüa-zÖÄÜ}={a-zÖÄÜA-Zöäü}'
+
+# for all completions: grouping / headline / ...
+zstyle ':completion:*:messages' format $'\e[01;35m -- %d -- \e[00;00m'
+zstyle ':completion:*:warnings' format $'\e[01;31m -- No Matches Found -- \e[00;00m'
+zstyle ':completion:*:descriptions' format $'\e[01;33m -- %d -- \e[00;00m'
+zstyle ':completion:*:corrections' format $'\e[01;33m -- %d -- \e[00;00m'
+
+# statusline for many hits
+zstyle ':completion:*:default' select-prompt $'\e[01;35m -- Match %M    %P -- \e[00;00m'
+
+# for all completions: show comments when present
+zstyle ':completion:*' verbose yes
+
+# in menu selection strg+space to go to subdirectories
+bindkey -M menuselect '^@' accept-and-infer-next-history
+
+# case-insensitive -> partial-word (cs) -> substring completion:
+zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'  
+
+# caching of completion stuff
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path "$ZSH_CACHE"
+
+# ~dirs: reorder output sorting: named dirs over userdirs
+zstyle ':completion::*:-tilde-:*:*' group-order named-directories users
+
+# ssh: reorder output sorting: user over hosts
+zstyle ':completion::*:ssh:*:*' tag-order "users hosts"
+
+# kill: advanced kill completion
+zstyle ':completion::*:kill:*:*' command 'ps xf -U $USER -o pid,%cpu,cmd'
+zstyle ':completion::*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;32'
+
+# rm: advanced completion (e.g. bak files first)
+zstyle ':completion::*:rm:*:*' file-patterns '*.o:object-files:object\ file *(~|.(old|bak|BAK)):backup-files:backup\ files *~*(~|.(o|old|bak|BAK)):all-files:all\ files'
+
+# vi: advanced completion (e.g. tex and rc files first)
+zstyle ':completion::*:vi:*:*' file-patterns 'Makefile|*(rc|log)|*.(php|tex|bib|sql|zsh|ini|sh|vim|rb|sh|js|tpl|csv|rdf|txt|phtml|tex|py|n3):vi-files:vim\ likes\ these\ files *~(Makefile|*(rc|log)|*.(log|rc|php|tex|bib|sql|zsh|ini|sh|vim|rb|sh|js|tpl|csv|rdf|txt|phtml|tex|py|n3)):all-files:other\ files'
+
+zstyle :compinstall filename '~/.zshrc'
+
+autoload -Uz compinit && compinit
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,24 +1,14 @@
-# Path to your oh-my-zsh installation.
-export ZSH="$HOME/.oh-my-zsh"
-
-# Uncomment the following line if you want to disable marking untracked files
-# under VCS as dirty. This makes repository status check for large repositories
-# much, much faster.
-DISABLE_UNTRACKED_FILES_DIRTY="true"
-
-COMPLETION_WAITING_DOTS="true"
+#               __
+#   ____  _____/ /_  __________
+#  /_  / / ___/ __ \/ ___/ ___/
+# _ / /_(__  ) / / / /  / /__
+#(_)___/____/_/ /_/_/   \___/
 
 # jump around
 eval "$(fasd --init auto)"
 
-# User configuration
-DEFAULT_USER="Kyle"
-
 # Enable rbenv
 if which rbenv > /dev/null; then eval "$(rbenv init - --no-rehash)"; fi
-
-# setup oh-my-zsh
-source $ZSH/oh-my-zsh.sh
 
 # Load pyenv on shell start
 if which pyenv > /dev/null 2>&1; then eval "$(pyenv init - --no-rehash)"; fi
@@ -29,72 +19,6 @@ if which pyenv-virtualenv-init > /dev/null 2>&1; then eval "$(pyenv virtualenv-i
 # setup nvm
 export NVM_DIR="$HOME/.nvm"
 source "/usr/local/opt/nvm/nvm.sh"
-
-# Enable vi editing mode and show in prompt
-function zle-keymap-select {
-  VIMODE="${${KEYMAP/vicmd/ M:command}/(main|viins)/}"
-  zle reset-prompt
-}
-
-zle -N zle-keymap-select
-bindkey -v
-set show-mode-in-prompt on
-
-# Vi mode breaks uparrow completion. Fix it.
-# start typing + [Up-Arrow] - fuzzy find history forward
-if [[ "${terminfo[kcuu1]}" != "" ]]; then
-  autoload -U up-line-or-beginning-search
-  zle -N up-line-or-beginning-search
-  bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
-fi
-# start typing + [Down-Arrow] - fuzzy find history backward
-if [[ "${terminfo[kcud1]}" != "" ]]; then
-  autoload -U down-line-or-beginning-search
-  zle -N down-line-or-beginning-search
-  bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
-fi
-
-# Enable changing cursor shapes and blinking
-function print_dcs
-{
-  print -n -- "\EP$1;\E$2\E\\"
-}
-
-function set_cursor_shape
-{
-  if [ -n "$TMUX" ]; then
-    # tmux will only forward escape sequences to the terminal if surrounded by
-    # a DCS sequence
-    print_dcs tmux "\E]50;CursorShape=$1\C-G"
-  else
-    print -n -- "\E]50;CursorShape=$1\C-G"
-  fi
-}
-
-function zle-keymap-select zle-line-init
-{
-  case $KEYMAP in
-    vicmd)
-      set_cursor_shape 0 # block cursor
-      ;;
-    viins|main)
-      set_cursor_shape 1 # line cursor
-      ;;
-  esac
-  zle reset-prompt
-  zle -R
-}
-
-function zle-line-finish
-{
-  set_cursor_shape 0 # block cursor
-}
-
-zle -N zle-line-init
-zle -N zle-line-finish
-zle -N zle-keymap-select
-
-bindkey -M menuselect '^[[Z' reverse-menu-complete
 
 # fzf initialization
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
@@ -108,9 +32,8 @@ source $ZPLUG_HOME/init.zsh
 # ------------ ZPLUG ------------------------
 zplug "mafredri/zsh-async",              from:github
 zplug "bhilburn/powerlevel9k",           from:github,   use:powerlevel9k.zsh-theme, as:theme
-zplug "plugins/pip",                     from:oh-my-zsh
-zplug "plugins/colored-man-pages",       from:oh-my-zsh
 zplug "unixorn/warhol.plugin.zsh"
+zplug "zsh-users/zsh-autosuggestions",   use:zsh-autosuggestions.zsh
 zplug "zsh-users/zsh-syntax-highlighting"
 
 zplug load
@@ -120,3 +43,4 @@ source ~/.exports
 source ~/.aliases
 source ~/.functions
 source ~/.extra
+source ~/.config.zsh
