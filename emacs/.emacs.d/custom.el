fadc61d4b0bbf07ba0611ffd99b3673af409685a
Kyle Cierzan <kylec@s898.ofc.lair>
2018-05-05 00:47:25 -0400

    Add custom file

---
+++ emacs/.emacs.d/custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(evil-collection-setup-minibuffer t)
+ '(package-selected-packages
+   (quote
+    (moe-theme yapfify yaml-mode xref-js2 ws-butler which-key wgrep web-mode vi-tilde-fringe use-package switch-window spacemacs-theme smart-mode-line shrink-path rjsx-mode rainbow-delimiters pyvenv pip-requirements php-extras pcmpl-pip pcmpl-git paradox org-bullets nose nlinum neotree multi-term markdown-mode json-mode imenu-list imenu-anywhere highlight-numbers highlight-indentation helm-xref helm-swoop helm-projectile helm-descbinds helm-ag hacker-typer git-gutter-fringe general flycheck-pos-tip exec-path-from-shell evil-visualstar evil-text-object-python evil-surround evil-snipe evil-smartparens evil-org evil-multiedit evil-matchit evil-magit evil-goggles evil-commentary evil-collection evil-anzu esup eshell-z eshell-prompt-extras esh-autosuggest emmet-mode elm-mode eldoc-eval dumb-jump doom-themes dockerfile-mode dired-k diminish company-tern company-statistics company-shell company-restclient company-quickhelp company-php company-anaconda beacon ace-link))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(evil-goggles-delete-face ((t (:inherit magit-diff-removed))))
+ '(evil-goggles-paste-face ((t (:inherit magit-diff-added))))
+ '(evil-goggles-undo-redo-add-face ((t (:inherit magit-diff-added))))
+ '(evil-goggles-undo-redo-remove-face ((t (:inherit magit-diff-removed))))
+ '(font-lock-comment-face ((t (:foreground "#6c6c6c" :slant italic))))
+ '(font-lock-string-face ((t (:foreground "#ff4ea3" :family "Iosevka Slab"))))
+ '(variable-pitch ((t (:slant normal :weight normal :height 160 :width normal :foundry "nil" :family "Bitter")))))
