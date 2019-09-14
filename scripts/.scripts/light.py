30da39166e67d2b80e5559c625a67a14c0e523f8
Kyle Cierzan <kcierzan@gmail.com>
2019-09-13 22:28:22 -0400

    Refactor light.py

--- scripts/.scripts/light.py
+++ scripts/.scripts/light.py
@@ -3,121 +3,60 @@ import copy
 import argparse
 import subprocess
 
-ROUNDING_FACTOR = 1
 
-GAMMA_VALUES = [
-    {
-        "red": 1.0,
-        "green": 0.2,
-        "blue": 0.1,
-        "temp": 1000
-    },
-    {
-        "red": 1.0,
-        "green": 0.3,
-        "blue": 0.1,
-        "temp": 1200
-    },
-    {
-        "red": 1.0,
-        "green": 0.4,
-        "blue": 0.1,
-        "temp": 1500
-    },
-    {
-        "red": 1.0,
-        "green": 0.5,
-        "blue": 0.1,
-        "temp": 2000
-    },
-    {
-        "red": 1.0,
-        "green": 0.6,
-        "blue":  0.3,
-        "temp": 2500
-    },
-    {
-        "red": 1.0,
-        "green": 0.7,
-        "blue":  0.4,
-        "temp": 3000
-    },
-    {
-        "red": 1.0,
-        "green": 0.8,
-        "blue": 0.5,
-        "temp": 3500
-    },
-    {
-        "red": 1.0,
-        "green": 0.8,
-        "blue": 0.6,
-        "temp": 4000
-    },
-    {
-        "red": 1.0,
-        "green": 0.9,
-        "blue": 0.8,
-        "temp": 5000
-    },
-    {
-        "red": 1.0,
-        "green": 1.0,
-        "blue": 0.9,
-        "temp": 6000
-    },
-    {
-        "red": 1.0,
-        "green": 1.0,
-        "blue": 1.0,
-        "temp": 6500
-    },
+PRESETS = [
+    {"temp": 1500, "gamma": [1.0, 0.5, 0.0]},
+    {"temp": 2000, "gamma": [1.0, 0.5, 0.2]},
+    {"temp": 2500, "gamma": [1.0, 0.6, 0.4]},
+    {"temp": 3500, "gamma": [1.0, 0.7, 0.5]},
+    {"temp": 4000, "gamma": [1.0, 0.8, 0.6]},
+    {"temp": 5000, "gamma": [1.0, 0.8, 0.8]},
+    {"temp": 6000, "gamma": [1.0, 1.0, 0.9]},
+    {"temp": 6500, "gamma": [1.0, 1.0, 1.0]},
 ]
+
+MAX_TEMP = PRESETS[-1]["temp"]
+MIN_TEMP = PRESETS[0]["temp"]
 
 class Display():
     def __init__(self):
-        self.display = self._get_connected_displays()
-        self.info = self._get_display_info()
+        self.display = self._get_display()
         self.brightness = self._get_brightness()
         self.gamma = self._get_gamma()
+        self.temperature = self.lookup_temp_by_gamma()
 
     def _shell_with_output(self, command):
         return subprocess.getoutput(command)
 
-    def _get_connected_displays(self):
-        return self._shell_with_output("xrandr | grep -w connected | cut -f1 -d ' '")
-
-    def _get_display_info(self):
-        display = self.display
-        xrandr = f"xrandr --verbose | grep -w {display} -A8"
-        output = self._shell_with_output(xrandr)
-        return output
+    def _get_display(self):
+        return self._shell_with_output(
+            "xrandr | grep -w connected | cut -f1 -d ' '")
 
     def _get_brightness(self):
         brightness = self._shell_with_output(
-            "echo '" + self.info + "' | grep Brightness | cut -f2 -d ' '")
-        return float(brightness) if brightness else None
+            "xrandr --verbose | " \
+            f"grep -w {self.display} -A8 | " \
+            " grep Brightness | " \
+            " cut -f2 -d ' '")
+        return float(brightness)
 
     def _get_gamma(self):
         gamma = self._shell_with_output(
-            "echo '" + self.info + "' | grep Gamma | cut -f2- -d: | tr -d ' '")
+            "xrandr --verbose | " \
+            f"grep -w {self.display} -A8 | " \
+            "grep Gamma | cut -f2- -d: | tr -d ' '")
         if gamma:
-            floats = [round(1 / float(value), 1)
-                      if  float(value) != 0.0 else 0.0
+            return [round(1 / float(value), 1)
+                      # don't try to divide by zero...
+                      if float(value) != 0.0 else 0.0
                     for value in gamma.split(":")]
-            return dict(zip(["red", "green", "blue"], floats))
         else:
             print("Gamma not found: {}".format(gamma))
 
-    def _input_format_gamma(self):
-        return ":".join([str(v) for v in self.gamma.values()])
-
     def lookup_temp_by_gamma(self):
-        for i, rgb in enumerate(GAMMA_VALUES):
-            rgb.pop("temp")
-            return(self.gamma)
-            if rgb.items() == self.gamma.items():
-                return GAMMA_VALUES[i]["temp"]
+        for preset in PRESETS:
+            if preset["gamma"] == self.gamma:
+                return preset["temp"]
 
     def change_brightness(self, operation):
         if operation == "+":
@@ -130,39 +69,28 @@ class Display():
                 new = 0.0
             else:
                 new = round(self.brightness - 0.1, 1)
-        cmd = f"redshift -P -o -b {new}:{new} -g {self._input_format_gamma()} 2> /dev/null"
+        cmd = f"redshift -P -O {self.temperature} -b {new}:{new} 2> /dev/null"
         self._shell_with_output(cmd)
         return new
 
     def change_temp(self, operation):
-        color_fields = ["red", "green", "blue"]
-        new_gamma = "1.0:1.0:1.0"
-        new_rgb = GAMMA_VALUES[10]
-        for i, rgb in enumerate(copy.deepcopy(GAMMA_VALUES)):
-            rgb.pop("temp")
-            if rgb == self.gamma:
+        for i, preset in enumerate(PRESETS):
+            if preset["gamma"] == self.gamma:
                 if operation == "+":
-                    if i + 1 > len(GAMMA_VALUES) - 1:
-                        i = len(GAMMA_VALUES) - 2
-                    new_rgb = GAMMA_VALUES[i + 1]
-                    new_gamma = ":".join(
-                       list(str(v) for k,v in
-                             GAMMA_VALUES[i + 1].items()
-                             if k in color_fields)
-                    )
+                    if i >= len(PRESETS) - 1:
+                        new_temp = MAX_TEMP
+                    else:
+                        new_temp = PRESETS[i + 1]["temp"]
                     break
                 elif operation == "-":
-                    if i - 1 < 0:
-                        i = 1
-                    new_rgb = GAMMA_VALUES[i - 1]
-                    new_gamma = ":".join(
-                        list(str(v) for k,v in
-                             GAMMA_VALUES[i - 1].items()
-                             if k in color_fields)
-                    )
+                    if i <= 1:
+                        new_temp = MIN_TEMP
+                    else:
+                        new_temp = PRESETS[i - 1]["temp"]
                     break
-        self._shell_with_output(f"redshift -P -o -b {self.brightness}:{self.brightness} -g {new_gamma}")
-        return new_rgb["temp"]
+        cmd = f"redshift -P -O {new_temp} -b {self.brightness}:{self.brightness}"
+        self._shell_with_output(cmd)
+        return new_temp
 
 
 def parse_arguments():
@@ -191,12 +119,10 @@ def main():
     args.action = args.action[0]
 
     if args.action == "info":
-        if args.target == "display":
-            print(display.display)
-        elif args.target == "brightness":
+        if args.target == "brightness":
             print(display.brightness)
         elif args.target == "temperature":
-            print(display.lookup_temp_by_gamma())
+            print(display.temperature)
         return
 
     if args.action != "info" and args.target == "display":
