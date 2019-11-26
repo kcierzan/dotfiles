b195450ee59b26f9191a844a0b201e57ba7df1ad
kylec <kylec@aweber.com>
2019-11-25 22:49:07 -0500

    Update weather signal

--- awesome/.config/awesome/signals/weather.lua
+++ awesome/.config/awesome/signals/weather.lua
@@ -6,6 +6,8 @@
 
 local awful = require("awful")
 local helpers = require("helpers")
+local gears = require("gears")
+local naughty = require("naughty")
 
 local darksky_api_key = os.getenv("DARKSKY_API_KEY")
 
@@ -13,43 +15,48 @@ local darksky_api_key = os.getenv("DARKSKY_API_KEY")
 local update_interval = 600
 
 -- call the `weather` shell script periodically
-awful.widget.watch("weather", update_interval, function(widget, stdout)
-                     local days = {}
-                     local ip = helpers.https.get("https://ifconfig.me")["body"]
-                     local latlon_res = helpers.https.get("http://ip-api.com/json/" .. ip)["body"]
+function update_forecast()
+  local days = {}
+  local ip = helpers.https.get("https://ifconfig.me")["body"]
+  local latlon_res = helpers.https.get("http://ip-api.com/json/" .. ip)["body"]
 
-                     if not (latlon_res) then
-                       -- fallback to Philadelphia
-                       local latlon = "39.93,-75.18"
-                     else
-                       local latlon = latlon_res["lat"] .. "," .. latlon_res["lon"]
-                     end
+  if not (latlon_res) then
+    -- fallback to Philadelphia
+    local latlon = "39.93,-75.18"
+  else
+    local latlon = latlon_res["lat"] .. "," .. latlon_res["lon"]
+  end
 
-                     local body = helpers.https.get(
-                       "https://api.darksky.net/forecast/"
-                       .. darksky_api_key
-                       .. "/"
-                       .. latlon
-                       .. "?=exclude=minutely")["body"]
+  local body = helpers.https.get(
+    "https://api.darksky.net/forecast/"
+    .. darksky_api_key
+    .. "/"
+    .. latlon
+    .. "?=exclude=minutely")["body"]
 
-                     if weather_res then
-                       -- insert the current info first
-                       table.insert(days, {
-                          icon = body["currently"]["icon"],
-                          temperature = body["currently"]["temperature"],
-                          summary = body["hourly"]["summmary"]
-                         })
+  if not body then
+    naughty.notify({title = "Fetch weather failed", text = "stopping weather callback"})
+    return false
+  end
 
-                       -- insert the rest of the day data
-                       for i, day in ipairs(body["daily"]["data"]) do
-                         table.insert(days, {
-                             icon = day["icon"],
-                             summary = day["summary"],
-                             low = day["temperatureLow"],
-                             high = day["temperatureHigh"]
-                           })
-                       end
-                     end
+    -- add the current weather info
+    table.insert(days, {
+        icon = body["currently"]["icon"],
+        temperature = body["currently"]["temperature"],
+        summary = body["hourly"]["summmary"]
+      })
 
-                     awesome.emit_signal("signals::weather", days)
-end)
+    -- add the rest of the forecast data
+    for i, day in ipairs(body["daily"]["data"]) do
+      table.insert(days, {
+          icon = day["icon"],
+          summary = day["summary"],
+          low = day["temperatureLow"],
+          high = day["temperatureHigh"]
+        })
+    end
+    awesome.emit_signal("signals::weather", days)
+    return true
+end
+
+gears.timer.start_new(update_interval, update_forecast)
