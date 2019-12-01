f4567121eaeb95bf4e20ba5737d6fbcfbd6d2048
Kyle Cierzan <kcierzan@gmail.com>
2019-12-01 17:45:13 -0500

    Update hdd bars more frequently

--- awesome/.config/awesome/signals/hdd.lua
+++ awesome/.config/awesome/signals/hdd.lua
@@ -1,7 +1,7 @@
 local awful = require("awful")
 local helpers = require("helpers")
 
-local update_interval = 60
+local update_interval = 5
 
 awful.widget.watch("hdd", update_interval, function(widget, stdout)
                      local lines = helpers.split(stdout, ":")
