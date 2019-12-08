2f4f1fc7d2db353e37adbb86d1a916b0d764050a
Kyle Cierzan <kcierzan@gmail.com>
2019-12-08 13:50:11 -0500

    Fix system widgets

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -57,6 +57,8 @@ local theme_collection = {
 -- current theme
 theme_name = theme_collection[1]
 
+widget_dir = "themes." .. theme_name .. ".widgets."
+
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(theme_dir .. theme_name .. "/theme.lua")
 
