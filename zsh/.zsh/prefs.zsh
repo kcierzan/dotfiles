be403a03f0866cb53beb928e1c1c7ee5351afe53
kylec <kylec@aweber.com>
2020-01-27 15:22:59 -0500

    Add additional zsh options

--- zsh/.zsh/prefs.zsh
+++ zsh/.zsh/prefs.zsh
@@ -1,8 +1,38 @@
 HISTFILE=$HOME/.zhistory
 HISTSIZE=10000000
 SAVEHIST=10000000
-setopt share_history append_history extended_history hist_no_store hist_ignore_all_dups hist_ignore_space
+setopt append_history hist_no_store hist_ignore_all_dups hist_ignore_space
 
+# If a command is issued that can’t be executed as a normal command,
+# and the command is the name of a directory, perform the cd command
+# to that directory.
+setopt AUTO_CD
+
+# Treat  the ‘#’, ‘~’ and ‘^’ characters as part of patterns for filename generation, etc.
+# (An initial unquoted ‘~’ always produces named directory expansion.)
+setopt EXTENDED_GLOB
+
+# If a pattern for filename generation has no matches, print an error, instead of
+# leaving it unchanged in the argument  list. This also applies to file expansion
+# of an initial ‘~’ or ‘=’.
+setopt NOMATCH
+
+# no Beep on error in ZLE.
+setopt NO_BEEP
+
+# Remove  any right prompt from display when accepting a command line.
+# This may be useful with terminals with other cut/paste methods.
+setopt TRANSIENT_RPROMPT
+
+# If unset, the cursor is set to the end of the word if completion is started.
+# Otherwise it stays there and completion is done from both ends.
+setopt COMPLETE_IN_WORD
+
+# Make cd push the old directory onto the directory stack.
+setopt AUTO_PUSHD
+
+# Don’t push multiple copies of the same directory onto the directory stack.
+setopt PUSHD_IGNORE_DUPS
 setopt COMBINING_CHARS      # Combine zero-length punctuation characters (accents)
 setopt INTERACTIVE_COMMENTS # Enable comments in interactive shell.
 setopt RC_QUOTES            # Allow 'Henry''s Garage' instead of 'Henry'\''s Garage'.
@@ -13,6 +43,18 @@ setopt NOTIFY               # Report status of background jobs immediately.
 unsetopt BG_NICE            # Don't run all background jobs at a lower priority.
 unsetopt HUP                # Don't kill jobs on shell exit.
 unsetopt CHECK_JOBS         # Don't report on jobs when shell exit.
+setopt BANG_HIST                 # Treat the '!' character specially during expansion.
+setopt EXTENDED_HISTORY          # Write the history file in the ":start:elapsed;command" format.
+setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
+setopt SHARE_HISTORY             # Share history between all sessions.
+setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
+setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS      # Delete old recorded entry if new entry is a duplicate.
+setopt HIST_FIND_NO_DUPS         # Do not display a line previously found.
+setopt HIST_IGNORE_SPACE         # Don't record an entry starting with a space.
+setopt HIST_SAVE_NO_DUPS         # Don't write duplicate entries in the history file.
+setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording entry.
+setopt HIST_VERIFY               # Don't execute immediately upon history expansion.
 
 zmodload zsh/terminfo
 typeset -gA key_info
