75deaa426b0c6a0a7038293d4c4138d3bd740410
Kyle Cierzan <kcierzan@gmail.com>
2022-03-18 23:12:30 -0400

    Clean up formatting

--- fish/.config/fish/functions/findfile.fish
+++ fish/.config/fish/functions/findfile.fish
@@ -1,15 +1,21 @@
 function findfile --description 'Fuzzy file finder'
-	set out (fd . --no-ignore --hidden --follow --type f \
-	--exclude ".git" \
-	--exclude "*.pyc" \
-	2> /dev/null \
-	| fzf +m --exit-0 --expect=ctrl-o,ctrl-x \
-	--preview-window=up:80% \
+	set out (
+    fd . \
+    --no-ignore \
+    --hidden \
+    --follow \
+    --type f \
+    --exclude ".git" \
+    --exclude "*.pyc" \
+    2> /dev/null \
+    | fzf +m --exit-0 --expect=ctrl-o,ctrl-x \
+    --preview-window=up:80% \
     --preview 'if test "(file --mime {} | string match -r 'binary')" != binary
-        	preview {} 2> /dev/null | head -2000
+          preview {} 2> /dev/null | head -2000
         else 
-        	echo {} is a binary file
-        end')
+          echo {} is a binary file
+        end'
+  )
 
 	set key (echo "$out" | cut -d' ' -f 1)
 	set file (echo "$out" | cut -d' ' -f 2)
