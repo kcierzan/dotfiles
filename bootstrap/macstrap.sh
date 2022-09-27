844d459b9571f919dc92a0f09fb2a9a91ce54607
Kyle Cierzan <kcierzan@gmail.com>
2022-09-27 09:51:27 -0400

    Improve macstrap readability

--- bootstrap/macstrap.sh
+++ bootstrap/macstrap.sh
@@ -58,13 +58,23 @@ if [ -n "$APPS" ]; then
 
   subtask_exec "Updating macOS" softwareupdate --install --all
 
-  [ -z "$(which brew)" ] && subtask_exec 'Installing homebrew' install_homebrew
+  [ -z "$(which brew)" ] &&
+    subtask_exec 'Installing homebrew' install_homebrew
 
-  is_tapped 'homebrew/cask' || subtask_exec 'Tapping cask' brew tap homebrew/cask
-  is_tapped 'homebrew/cask-versions' || subtask_exec 'Tapping cask versions' brew tap homebrew/cask-versions
-  is_tapped 'homebrew/cask-fonts' || subtask_exec 'Tapping cask fonts' brew tap homebrew/cask-fonts
-  is_tapped 'homebrew/services' || subtask_exec 'Tapping cask services' brew tap homebrew/services
-  is_tapped 'felixkratz/formulae' || subtask_exec 'Tapping sketchybar' brew tap felixkratz/formulae
+  is_tapped 'homebrew/cask' ||
+    subtask_exec 'Tapping cask' brew tap homebrew/cask
+
+  is_tapped 'homebrew/cask-versions' ||
+    subtask_exec 'Tapping cask versions' brew tap homebrew/cask-versions
+
+  is_tapped 'homebrew/cask-fonts' ||
+    subtask_exec 'Tapping cask fonts' brew tap homebrew/cask-fonts
+
+  is_tapped 'homebrew/services' ||
+    subtask_exec 'Tapping cask services' brew tap homebrew/services
+
+  is_tapped 'felixkratz/formulae' ||
+    subtask_exec 'Tapping sketchybar' brew tap felixkratz/formulae
 
   subtask_exec 'Installing homebrew packages' install_brew_packages
 
@@ -72,9 +82,11 @@ if [ -n "$APPS" ]; then
 
   subtask_exec 'Upgrading homebrew packages' brew_upgrade
 
-  [ -z "$(which fennel)" ] && subtask_exec 'Installing fennel' luarocks install fennel
+  [ -z "$(which fennel)" ] && 
+    subtask_exec 'Installing fennel' luarocks install fennel
 
-  mas list | grep -q 'Things' || subtask_exec 'Installing Things' mas install "$THINGS3_APP_STORE_ID"
+  mas list | grep -q 'Things' ||
+    subtask_exec 'Installing Things' mas install "$THINGS3_APP_STORE_ID"
 fi
 
 # --------------------------------------------------------------
@@ -85,13 +97,16 @@ if [ -n "$CONFIG_SHELL" ]; then
 
   subtask_exec 'Symlinking dotfiles' stow_dot_dirs
 
-  [ -z "$(pgrep -i hammerspoon)" ] && subtask_exec 'Starting hammerspoon' open /Applications/Hammerspoon.app
+  [ -z "$(pgrep -i hammerspoon)" ] &&
+    subtask_exec 'Starting hammerspoon' open /Applications/Hammerspoon.app
 
-  [ "$SHELL" != "$(brew --prefix)/bin/fish" ] && subtask_exec "Changing $(whoami)'s shell to fish" chsh -s "$(which fish)"
+  [ "$SHELL" != "$(brew --prefix)/bin/fish" ] &&
+    subtask_exec "Changing $(whoami)'s shell to fish" chsh -s "$(which fish)"
 
   subtask_exec 'Setting fish global variables' set_fish_globals
 
-  [ -z "$(fish -c 'fisher -v')" ] && subtask_exec 'Installing fisher' install_fisher
+  [ -z "$(fish -c 'fisher -v')" ] &&
+    subtask_exec 'Installing fisher' install_fisher
 
   subtask_exec 'Symlinking bootstrap script' symlink_bootstrap_executable
   
@@ -103,12 +118,16 @@ if [ -n "$RUNTIMES" ]; then
 
   subtask_exec 'Installing asdf plugins' install_asdf_plugins
 
-  asdf list ruby | grep -q $GLOBAL_RUBY_VERSION && subtask_exec "Installing ruby $GLOBAL_RUBY_VERSION" asdf install ruby "$GLOBAL_RUBY_VERSION"
-  asdf list python | grep -q $GLOBAL_PYTHON_VERSION && subtask_exec "Installing python $GLOBAL_PYTHON_VERSION" asdf install python "$GLOBAL_PYTHON_VERSION"
+  asdf list ruby | grep -q $GLOBAL_RUBY_VERSION &&
+    subtask_exec "Installing ruby $GLOBAL_RUBY_VERSION" asdf install ruby "$GLOBAL_RUBY_VERSION"
+
+  asdf list python | grep -q $GLOBAL_PYTHON_VERSION &&
+    subtask_exec "Installing python $GLOBAL_PYTHON_VERSION" asdf install python "$GLOBAL_PYTHON_VERSION"
 
   subtask_exec 'Setting global ruby version' asdf global ruby "$GLOBAL_RUBY_VERSION"
 
-  [ -z "$(asdf which bundler)" ] && subtask_exec 'Installing bundler' asdf exec gem install bundler
+  [ -z "$(asdf which bundler)" ] &&
+    subtask_exec 'Installing bundler' asdf exec gem install bundler
 
   if [ ! -d ~/.inkd/ ]; then
     [ ! -d ~/git/inkd/ ] &&
