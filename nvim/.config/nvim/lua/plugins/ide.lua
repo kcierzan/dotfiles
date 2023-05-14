2e8772beb75d432d33a2c63597ca7fa59a914347
Kyle Cierzan <kcierzan@gmail.com>
2023-05-14 00:17:55 -0400

    Fix toggleterm

--- nvim/.config/nvim/lua/plugins/ide.lua
+++ nvim/.config/nvim/lua/plugins/ide.lua
@@ -6,6 +6,7 @@ return {
   {
     "akinsho/toggleterm.nvim",
     keys = { "<C-\\>" },
+    cmd = { "ToggleTerm" },
     config = function()
       require("toggleterm").setup({ open_mapping = "<C-\\>" })
     end
