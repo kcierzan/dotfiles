551a1a70e4d36a71169e7b566b14143bb10987e0
Kyle Cierzan <kcierzan@gmail.com>
2023-06-22 11:01:43 -0400

    Add rebase script

---
+++ fish/.config/fish/functions/rlm.fish
@@ -0,0 +1,16 @@
+function rlm --description='Update default branch from remote and rebase the current branch onto it'
+  set upstream (git remote)
+  set current_branch (git branch --show-current)
+  set default_branch (git symbolic-ref "refs/remotes/$upstream/HEAD" | sed "s@^refs/remotes/$upstream/@@")
+
+  git stash
+
+  if test $current_branch = $default_branch
+    git pull $upstream $current_branch
+  else
+    git fetch $upstream "$default_branch:$default_branch"
+    git rebase $default_branch
+  end
+
+  git stash pop
+end
