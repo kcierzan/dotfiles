68c81bcda85d9310dfbf7d201db09703d615538c
Kyle Cierzan <kcierzan@gmail.com>
2024-03-14 22:39:25 -0400

    Disable none-ls ruff

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -224,7 +224,7 @@ return {
       null_ls.setup({
         sources = {
           null_ls.builtins.formatting.stylua,
-          null_ls.builtins.diagnostics.ruff,
+          -- null_ls.builtins.diagnostics.ruff,
           null_ls.builtins.diagnostics.rubocop.with({
             command = "bundle",
             args = {"exec", "rubocop", "-f", "json", "--force-exclusion", "--stdin", "$FILENAME"}
