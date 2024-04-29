f7b7974942339bfd531ace637b03198545245e12
Kyle Cierzan <kcierzan@gmail.com>
2024-04-28 20:06:23 -0400

    Add emacs bin to fish path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,4 @@
-fish_add_path "$HOME/.local/bin" /opt/homebrew/bin /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /sbin "$HOME/.cargo/bin"
+fish_add_path "$HOME/.local/bin" /opt/homebrew/bin /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /sbin "$HOME/.cargo/bin" "$HOME/.emacs.d/bin"
 
 if test (uname -s) = 'Linux'
   fish_add_path /var/lib/snapd/snap/bin
