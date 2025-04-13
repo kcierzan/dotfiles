d45d3313cd85cfc7ed8ec052935f4e2dbdc38dcb
Kyle Cierzan <kcierzan@gmail.com>
2025-04-13 12:06:44 -0400

    Add cattpuccin colorschemes

--- private_dot_config/nvim/lua/plugins/catppuccin.lua
+++ private_dot_config/nvim/lua/plugins/catppuccin.lua
@@ -1,3 +1,5 @@
+local overrides = require("catppuccin_xcode")
+
 return {
   "catppuccin/nvim",
   name = "catppuccin",
@@ -10,36 +12,7 @@ return {
           SnacksPickerBorder = { bg = colors.mantle },
         }
       end,
-      color_overrides = {
-        all = {
-          rosewater = "#d8dee9",
-          flamingo = "#d8dee9",
-          pink = "#d8dee9",
-          mauve = "#c695c6",
-          red = "#ec5f66",
-          maroon = "#9f5a5a",
-          peach = "#f07f62",
-          yellow = "#f9ae58",
-          green = "#99c794",
-          teal = "#5fb4b4",
-          sky = "#89dceb",
-          sapphire = "#6db4ce",
-          blue = "#6699cc",
-          lavender = "#c695c6",
-          text = "#d8dee9",
-          subtext1 = "#c5cede",
-          subtext0 = "#a9b6cc",
-          overlay2 = "#97a6bf",
-          overlay1 = "#8494af",
-          overlay0 = "#72829d",
-          surface2 = "#606d7b",
-          surface1 = "#55626f",
-          surface0 = "#4b5763",
-          base = "#343d46",
-          mantle = "#282f36",
-          crust = "#15191e",
-        },
-      },
+      color_overrides = overrides.color_overrides,
     })
 
     vim.cmd.colorscheme("catppuccin")
