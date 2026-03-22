d2bc2cbd0246daa6c23e986556b79430cd1abe48
Kyle Cierzan <kcierzan@gmail.com>
2026-03-22 14:26:51 -0400

    Short circuit mise activation

--- private_dot_config/zsh/lib/mise.zsh
+++ private_dot_config/zsh/lib/mise.zsh
@@ -1,3 +1,5 @@
 if (( $+commands[mise] )); then
-  eval "$(mise activate zsh)"
+  # Don't activate mise if the bootstrap directory exists as the config files
+  # in there will set up mise and this will just slow down shell startup
+  [[ ! -d "$HOME/.bootstrap" ]] && eval "$(mise activate zsh)"
 fi
